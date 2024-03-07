ecb7ff2372019402ee8693f8d2707ec627acf9c8
Suhail Jamal <119126634+SuhailJamal@users.noreply.github.com>
2024-03-07 18:08:21 +0530

    Starting the Front End

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express  = require("express")
 const app = express();
 app.get('/',(req,res)=>{
-    res.send("hello fahad")
+    res.send("hello bro")
 })
 app.listen(3000,()=>{
     console.log("app listening on 3000 port")
